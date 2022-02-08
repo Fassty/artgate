5972f5750d66e297d16e57df9821c512c94b83eb
mikulaspa <mikulaspa@gmail.com>
2022-02-08 20:36:12 +0100

    More refactoring -> move slider functionality to code, minimize and exit functionality

--- artgate/widgets/buttons.py
+++ artgate/widgets/buttons.py
@@ -9,6 +9,7 @@ IMAGES = load_images_to_categories('data/images/zdroje')
 ACTIVE_CATEGORIES = {cat: False for cat in CATEGORIES}
 
 from artgate.behaviors.hover_behavior import HoverBehavior
+from kivy.app import App
 
 
 class StrokeButtonSmall(HoverBehavior, Button):
@@ -20,17 +21,14 @@ class StrokeButtonSmall(HoverBehavior, Button):
     def on_enter(self):
         if not self.active:
             self.canvas.before.children[0].a = 0.5
-            print("I'm in")
 
     def on_leave(self):
         if not self.active:
             self.canvas.before.children[0].a = 0
-            print("I'm out")
 
     def on_release(self):
         if not self.active:
             self.canvas.before.children[0].a = 0.5
-            print("I'm in")
 
     def on_press(self):
         self.active = not self.active
@@ -38,16 +36,28 @@ class StrokeButtonSmall(HoverBehavior, Button):
             self.canvas.before.children[0].a = 0.75
             category = str(self.text).lower()
             ACTIVE_CATEGORIES[category] = True
-            print(ACTIVE_CATEGORIES)
             img = IMAGES[category][0]
             ctypes.windll.user32.SystemParametersInfoW(20, 0, img, 3)
         else:
             self.canvas.before.children[0].a = 0
             ACTIVE_CATEGORIES[str(self.text).lower()] = False
-            print(ACTIVE_CATEGORIES)
 
 
 class StrokeButtonLarge(Button):
     stroke_width = 1.1
     stroke_radius = 20
     stroke_color = (0, 0, 0, 1)
+
+
+class MinimizeButton(StrokeButtonSmall):
+    text = '__'
+
+    def on_press(self):
+        App.get_running_app().root_window.minimize()
+
+
+class ExitButton(StrokeButtonSmall):
+    text = 'X'
+
+    def on_press(self):
+        App.get_running_app().stop()
--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from artgate.constants import CATEGORIES
 from artgate.platform.utils import get_platform_connector
 
 connector = get_platform_connector()
+screen_x, screen_y = connector.get_screen_size()
+# TODO: overlay size based on screen dims
+overlay_x, overlay_y = 900, 500
 
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'fullscreen', False)
-Config.set('graphics', 'width', 900)
-Config.set('graphics', 'height', 500)
+Config.set('graphics', 'width', overlay_x)
+Config.set('graphics', 'height', overlay_y)
 Config.set('graphics', 'shaped', 1)
 Config.set('graphics', 'position', 'custom')
 
@@ -24,29 +27,26 @@ from kivy.core.window import Window
 # Has to be imported after HoverBehavior
 # noinspection PyUnresolvedReferences
 from artgate.widgets.buttons import StrokeButtonSmall, StrokeButtonLarge
+# noinspection PyUnresolvedReferences
+from artgate.widgets.sliders import OverlaySlider
 
 IMAGES = load_images_to_categories('data/images/zdroje')
 ACTIVE_CATEGORIES = {cat: False for cat in CATEGORIES}
 
 
 class MainScreen(Widget):
-    def slide_it(self, *args):
-        val = args[1]
-        if val == 0:
-            self.slider_image = 'data/images/slider_cropped.png'
-        else:
-            self.slider_image = 'data/images/slider_cropped_flipped.png'
+    pass
 
 
 class ArtGateApp(App):
 
     def build(self):
-        screenx, screeny = connector.get_screen_size()
-        Window.size = (900, 500)
-        Window.shape_image = 'data/images/window_mask900x500.png'
+        Window.size = (overlay_x, overlay_y)
+        Window.left = (screen_x - overlay_x) / 2
+        Window.top = (screen_y - overlay_y) / 2
+        Window.shape_image = f'data/images/window_mask{overlay_x}x{overlay_y}.png'
         Window.shape_mode = 'binalpha'
-        Window.left = (screenx - 900) / 2
-        Window.top = (screeny - 500) / 2
+        Window.borderless = True
         return MainScreen()
 
 
